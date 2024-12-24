571a26defcb371cff9792401547b01183cf82c2c
覃桃勇 <2294855999@qq.com>
2024-12-24 23:11:58 +0800

    循环任务调度

---
+++ simple-common-test/src/main/java/com/simple/common/test/SimpleCommonTestApplication.java
@@ -0,0 +1,13 @@
+package com.simple.common.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCommonTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCommonTestApplication.class, args);
+    }
+
+}
