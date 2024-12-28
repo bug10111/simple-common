deb28b71a1a8c938bf52fd4b23ec00b6353e29be
覃桃勇 <2294855999@qq.com>
2024-12-28 21:23:19 +0800

    缓存

---
+++ simple-common-test/src/main/java/com/simple/common/test/controller/RedisCacheController.java
@@ -0,0 +1,36 @@
+package com.simple.common.test.controller;
+
+import com.simple.common.core.response.R;
+import com.simple.common.redis.annotation.RedisCache;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA
+ *
+ * @author 兄台丶请冷静
+ */
+@Slf4j
+@RequestMapping("redis")
+@Tag(name = "分布式缓存")
+@RestController
+public class RedisCacheController {
+
+    @Operation(summary = "分布式缓存")
+    @PostMapping("cache")
+    @RedisCache
+    public R<List<String>> list() {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add("测试数据：" + i);
+        }
+        return R.ok(list);
+    }
+}
