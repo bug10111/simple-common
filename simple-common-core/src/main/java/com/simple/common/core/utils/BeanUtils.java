72e6840b917872ef3802d3ff75e0c27c0cc4c0d4
覃桃勇 <2294855999@qq.com>
2025-05-23 10:18:13 +0800

    bean复制更新

--- simple-common-core/src/main/java/com/simple/common/core/utils/BeanUtils.java
+++ simple-common-core/src/main/java/com/simple/common/core/utils/BeanUtils.java
@@ -3,10 +3,8 @@ package com.simple.common.core.utils;
 import cn.hutool.core.bean.BeanUtil;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cglib.beans.BeanCopier;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created with IntelliJ IDEA
@@ -16,9 +14,6 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 @Slf4j
 public class BeanUtils extends BeanUtil {
-
-    // 新增缓存（线程安全）
-    private static final ConcurrentHashMap<String, BeanCopier> BEAN_COPIER_CACHE = new ConcurrentHashMap<>();
 
     /**
      * 将对象转化为map
@@ -42,26 +37,41 @@ public class BeanUtils extends BeanUtil {
         return fillBeanWithMap(map, t, true);
     }
 
-    /**
-     * 基于 CGLIB，性能较高
-     *
-     * @param source 源数据
-     * @param tClass 目标对象
-     */
-    @SneakyThrows
-    public static <T> T copyProperties(Object source, Class<T> tClass) {
-        if (source == null) {
-            return null;
-        }
+    // 新增缓存（线程安全）
+    //    private static final ConcurrentHashMap<String, BeanCopier> BEAN_COPIER_CACHE = new ConcurrentHashMap<>();
 
-        // 获取或创建 BeanCopier（线程安全）
-        Class<?> sourceClass = source.getClass();
-        String cacheKey = sourceClass.getName() + "->" + tClass.getName();
-        BeanCopier copier = BEAN_COPIER_CACHE.computeIfAbsent(cacheKey, key -> BeanCopier.create(sourceClass, tClass, false));
+    //    /**
+    //     * 基于 CGLIB，性能较高,但是和@Accessors(chain = true)冲突，无法复制属性
+    //     *
+    //     * @param source 源数据
+    //     * @param tClass 目标对象
+    //     */
+    //    @SneakyThrows
+    //    public static <T> T copyProperties(Object source, Class<T> tClass) {
+    //        if (source == null) {
+    //            return null;
+    //        }
+    //
+    //        // 获取或创建 BeanCopier（线程安全）
+    //        Class<?> sourceClass = source.getClass();
+    //        String cacheKey = sourceClass.getName() + "->" + tClass.getName();
+    //        BeanCopier copier = BEAN_COPIER_CACHE.computeIfAbsent(cacheKey, key -> BeanCopier.create(sourceClass, tClass, true));
+    //
+    //        // 实例化并拷贝
+    //        T target = tClass.getDeclaredConstructor().newInstance();
+    //        copier.copy(source, target, new IgnoreReturnTypeConverter());
+    //        return target;
+    //    }
 
-        // 实例化并拷贝
-        T target = tClass.getDeclaredConstructor().newInstance();
-        copier.copy(source, target, null);
-        return target;
-    }
+    //    /**
+    //     * 自定义 Converter：忽略 Setter 的返回类型
+    //     * 解决了@Accessors(chain = true)冲突，但是同名称不同属性类型会报错
+    //     */
+    //    private static class IgnoreReturnTypeConverter implements Converter {
+    //        @Override
+    //        public Object convert(Object sourceValue, Class targetType, Object context) {
+    //            // 直接返回原值，BeanCopier 会尝试调用参数类型匹配的 Setter（无论返回类型是否 void）
+    //            return sourceValue;
+    //        }
+    //    }
 }
