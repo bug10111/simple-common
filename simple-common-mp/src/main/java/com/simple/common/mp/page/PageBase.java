7020498a198285b6a5e69f9206798603a849b2be
qty <2294855999@qq.com>
2026-04-26 10:24:05 +0800

    refactor(page): 优化分页参数基类设计
    - 更新类注释，明确分页参数基类的功能和用途
    - 添加详细的类功能说明文档
    - 重构 getPage() 方法，提取为私有的 createPage() 内部方法
    - 新增带类型参数的 getPage(Class<T> clazz) 重载方法
    - 优化排序处理逻辑，统一在 createPage() 方法中处理
    - 删除废弃的带 Class 参数的旧版 getPage 方法
    - 改进内部排序逻辑处理流程

--- simple-common-mp/src/main/java/com/simple/common/mp/page/PageBase.java
+++ simple-common-mp/src/main/java/com/simple/common/mp/page/PageBase.java
@@ -4,7 +4,6 @@ import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.core.toolkit.sql.SqlInjectionUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.simple.common.core.response.R;
 import com.simple.common.core.utils.AssertUtils;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
@@ -15,7 +14,10 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA
+ * 分页参数基类
+ * <p>
+ * 提供通用的分页参数（当前页、每页大小、排序），支持动态排序和SQL注入检测。
+ * 业务查询对象可继承此类，通过 getPage() 方法获取 MyBatis-Plus 的 Page 对象。
  *
  * @author qty
  */
@@ -37,10 +39,34 @@ public class PageBase {
     /**
      * 获取分页参数对象
      *
-     * @return 分页对象
+     * @return 分页对象，泛型类型为未知类型
      */
     public Page<?> getPage() {
-        Page<?> tPage = new Page<>(this.current, this.size);
+        return createPage();
+    }
+
+    /**
+     * 获取指定类型的分页参数对象
+     *
+     * @param clazz 实体类类型
+     * @param <T>   实体类型
+     * @return 分页对象，泛型类型为指定的实体类型
+     */
+    public <T> Page<T> getPage(Class<T> clazz) {
+        return createPage();
+    }
+
+    /**
+     * 创建分页对象并设置排序规则
+     * <p>
+     * 内部方法，处理分页参数和排序逻辑，包含SQL注入检测。
+     * 排序格式：字段名-true/false，true为正序，false为倒序，字段名为驼峰命名。
+     *
+     * @param <T> 实体类型
+     * @return 配置好分页参数和排序规则的 Page 对象
+     */
+    private <T> Page<T> createPage() {
+        Page<T> tPage = new Page<>(this.current, this.size);
         List<OrderItem> orders = Arrays.stream(sort).map(s -> {
             String[] split = s.split("-");
             AssertUtils.isTrue(split.length == 2, "排序字段格式必须为：字段名-true/false");
@@ -50,34 +76,6 @@ public class PageBase {
                 AssertUtils.error("请求错误");
             } else {
                 item.setColumn(StrUtil.toUnderlineCase(StrUtil.replace(split[0], " ", "")));
-            }
-            item.setAsc(Boolean.parseBoolean(split[1]));
-
-            return item;
-        }).toList();
-
-        tPage.setOrders(orders);
-        return tPage;
-    }
-
-    /**
-     * 获取分页参数对象
-     *
-     * @param tClass 预留
-     * @return 分页对象
-     */
-    @Deprecated
-    public <R> Page<R> getPage(Class<R> tClass) {
-        Page<R> tPage = new Page<>(this.current, this.size);
-        List<OrderItem> orders = Arrays.stream(sort).map(s -> {
-            String[] split = s.split("-");
-            AssertUtils.isTrue(split.length == 2, "排序字段格式必须为：字段名-true/false");
-            OrderItem item = new OrderItem();
-            if (SqlInjectionUtils.check(split[0])) {
-                log.error("动态排序失败！检测到SQL注入语句 [{}]", split[0]);
-                AssertUtils.error("请求错误");
-            } else {
-                item.setColumn(StrUtil.toUnderlineCase(split[0]));
             }
             item.setAsc(Boolean.parseBoolean(split[1]));
 
